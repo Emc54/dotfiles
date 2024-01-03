485fa22035aeb10faafeb6414c9dd2e27e205c89
Emc54 <35582806+Emc54@users.noreply.github.com>
2024-01-03 17:07:00 +0000

    initial commit of experimental nvim files

---
+++ nvim/lua/emc/init.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+require("emc.lazy")
+require("emc.remap")
+require("emc.set")
+require("emc.treesitter")
+vim.cmd("colorscheme kanagawa-dragon")
---
+++ nvim/lua/emc/plugins/init.lua
@@ -0,0 +1,9 @@
+return  {
+    'rebelot/kanagawa.nvim',
+	'tpope/vim-fugitive',
+	'mbbill/undotree',
+    'ThePrimeagen/vim-be-good',
+    {'stevearc/dressing.nvim',
+        event = "VeryLazy",
+    }
+}
