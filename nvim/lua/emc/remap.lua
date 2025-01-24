814d3cf7cd297195784e97171d17a0dd04a55272
Emc54 <35582806+Emc54@users.noreply.github.com>
2025-01-24 22:22:50 +0000

    Add nvim-tree and disable netrw

--- nvim/lua/emc/remap.lua
+++ nvim/lua/emc/remap.lua
@@ -1,5 +1,3 @@
-vim.keymap.set("n", "<leader>sv", vim.cmd.Ex)
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -12,9 +10,9 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- vim.keymap.set("v", "<leader>y", "\"+y")
 -- vim.keymap.set("n", "<leader>Y", "\"+Y")
 
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Open git status" })
 
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle Undo Tree" })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
